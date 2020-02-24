9145f53559ed5b804998c6539cf9753084690df7
Manuel Salinas <msalinasdiaz@gmail.com>
2020-02-24 16:59:24 -0300

    crea controlador users, guarda usuario creado, crea cierre sesion

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :older, presence: true
+  :recoverable, :rememberable, :validatable
+  validates :older, presence: true  #:authentication_keys => [:username]
+  
+  # Setup accessible (or protected) attributes for your model
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  # attr_accessible :title, :body
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :pubs
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+
   get 'home/index'
   root 'pubs#index'
+  
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
